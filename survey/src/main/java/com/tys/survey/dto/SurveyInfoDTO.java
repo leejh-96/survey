f05bc40b83bf55007e9ebbb9cee90e44d5b1db2e
leejh-96 <sbxl12@naver.com>
2023-10-18 21:28:50 +0900

    [feat] Survey top parent DTO completion

---
+++ survey/src/main/java/com/tys/survey/dto/SurveyInfoDTO.java
@@ -0,0 +1,49 @@
+package com.tys.survey.dto;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter@Setter@Builder
+public class SurveyInfoDTO{
+
+    private Integer surveyNo;
+
+    private Integer memberNo;
+
+    private String surveyTitle;
+
+    private Integer surveyCount;
+
+    private MultipartFile file;
+
+    private String thumbnailName;
+
+    private String thumbnailRename;
+
+    private String thumbnailPath;
+
+    private String surveyPublic;
+
+    private Integer surveyHit;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate surveyDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate surveyEndDate;
+
+    private LocalDateTime surveyWriteTime;
+
+    private Integer resultCount;
+
+    List<QuestionDTO> questionList;
+
+    public SurveyInfoDTO() {
+    }
+}
