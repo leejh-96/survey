4f97a2d8a4cc32d743827c0ab79a497bc3aeef0d
leejh-96 <sbxl12@naver.com>
2023-10-26 11:13:44 +0900

    [repactor] add lombok tostring

--- survey/src/main/java/com/tys/survey/dto/SurveyDTO.java
+++ survey/src/main/java/com/tys/survey/dto/SurveyDTO.java
@@ -2,6 +2,9 @@ package com.tys.survey.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+@ToString(callSuper = true)
 @Getter@Setter
 public class SurveyDTO extends SurveyInfoDTO{
 
--- survey/src/main/java/com/tys/survey/dto/SurveyFormDTO.java
+++ survey/src/main/java/com/tys/survey/dto/SurveyFormDTO.java
@@ -2,10 +2,11 @@ package com.tys.survey.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
+@ToString
 @Getter@Setter
 public class SurveyFormDTO {
 
